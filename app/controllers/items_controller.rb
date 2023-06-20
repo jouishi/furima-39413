69c43e432463535b16a6579f4c4bc35abaa9f341
jouishi <08251023jke01@gmail.com>
2023-06-20 18:25:43 +0900

    rubocop済

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :editA, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,10 +23,32 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'items/edit'
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id,
                                  :item_prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id: current_user.id)
+  end
+
+  def editA
+    @item = Item.find(params[:id])
+
+    return if @item.user == current_user
+
+    redirect_to root_path
   end
 end
