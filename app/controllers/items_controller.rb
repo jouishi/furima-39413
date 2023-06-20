ad353740a84175112b51eecd661c7ed3e497ab48
jouishi <08251023jke01@gmail.com>
2023-06-20 19:01:38 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :editA, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :check_edit_permission, only: [:edit, :update]
 
   def index
-    @items = Item.order('created_at DESC')
+    @items = Item.order(created_at: :desc)
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -24,31 +24,30 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
-      render 'items/edit'
+      render :edit
     end
   end
 
   private
 
-  def item_params
-    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id,
-                                 :item_prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id: current_user.id)
+  def set_item
+    @item = Item.find(params[:id])
   end
 
-  def editA
-    @item = Item.find(params[:id])
-
+  def check_edit_permission
     return if @item.user == current_user
 
     redirect_to root_path
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id,
+                                 :item_prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id: current_user.id)
   end
 end
