520b54e4d333403abcec70ecb4ab118d88b4398d
jouishi <08251023jke01@gmail.com>
2023-07-02 18:39:30 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.3'
 set :application, 'furima-39413'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:jyouishi/furima-39413.git'
+set :repo_url,  'git@github.com:jouishi/furima-39413.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
