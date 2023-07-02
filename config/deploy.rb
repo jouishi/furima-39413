756fbe48c98bf36341a749648472da0bed17053c
jouishi <08251023jke01@gmail.com>
2023-07-02 19:58:41 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :application, 'furima-39413'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:jouishi/furima-39413.git'
 
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
